69dd6e7f2ce92917742f729ac0e4ff3d50d8b91c
marciodelfinooliveira <marciodelfinooliveira@gmail.com>
2023-09-01 16:49:14 -0300

    Limpando a branch Master

--- antecessor_sucessor.py
+++
@@ -1,9 +0,0 @@
-while True:    
-    numero = int(input("Insira o número: "))
-    print(f"\nSucessor: {numero+1}\nAntecessor: {numero-1}")
-    
-    continuar = input("\nDeseja Continuar ? (Não = Qualquer tecla)(Sim = s): \n")
-    if continuar in ["s", "S"]:
-        continue
-    else:
-        break
