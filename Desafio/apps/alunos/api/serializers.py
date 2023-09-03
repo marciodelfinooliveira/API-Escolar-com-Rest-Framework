694d0e2797df79e0e07bb5aee56812cbba5b1b3f
marciodelfinooliveira <marciodelfinooliveira@gmail.com>
2023-09-02 21:25:16 -0300

    "VERSÃO: 1.0.5" | README: 1.0.1

--- Desafio/apps/alunos/api/serializers.py
+++ Desafio/apps/alunos/api/serializers.py
@@ -5,4 +5,4 @@ class AlunoSerializer(serializers.ModelSerializer):
     curso_nome_curso = serializers.CharField(source='curso.nome_curso', read_only=True)
     class Meta:
         model = AlunoModel
-        fields = ('id', 'nome','cpf', 'email', 'data_nascimento', 'curso_nome_curso')
+        fields = ('id', 'nome','cpf', 'email', 'data_nascimento', 'curso', 'curso_nome_curso')
