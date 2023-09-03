124845f632e3043e1fb59897e72f9700d1025f54
marciodelfinooliveira <marciodelfinooliveira@gmail.com>
2023-09-02 21:20:42 -0300

    "VERSÃO: 1.0.4" | README: 1.0.1

--- Desafio/apps/alunos/api/serializers.py
+++ Desafio/apps/alunos/api/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from apps.alunos.models import AlunoModel
 
 class AlunoSerializer(serializers.ModelSerializer):
+    curso_nome_curso = serializers.CharField(source='curso.nome_curso', read_only=True)
     class Meta:
         model = AlunoModel
-        fields = ('id', 'nome','cpf', 'email', 'data_nascimento', 'curso')
+        fields = ('id', 'nome','cpf', 'email', 'data_nascimento', 'curso_nome_curso')
--- Desafio/apps/cursos/api/serializers.py
+++ Desafio/apps/cursos/api/serializers.py
@@ -2,7 +2,8 @@ from rest_framework import serializers
 from apps.cursos.models import CursoModel
 
 class CursoSerializer(serializers.ModelSerializer):
-    
+    professor_nome = serializers.CharField(source='professor.nome', read_only=True)
+
     class Meta:
         model = CursoModel
-        fields = ('id', 'nome_curso',  'codigo_curso', 'descricao','carga_horaria', 'turno', 'nivel', 'professor')
+        fields = ('id', 'nome_curso', 'codigo_curso', 'descricao', 'carga_horaria', 'turno', 'nivel', 'professor_nome')
